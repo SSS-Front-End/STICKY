abd8f41dfda6d8a41ca58d53bfa3af8278f21124
SSS-dev <s.s.s.front.end.developer@gmail.com>
2025-05-24 20:11:29 +0500

    promo preloadSettings

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const loader = window.addEventListener('load', function () {
+  document.getElementById('preloader').style.display = 'none';
+  document.getElementById('main-content').style.display = 'block';
+});
